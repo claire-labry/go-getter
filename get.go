6851a67882a2446cd60360275a915a4bc37f5d0d
Claire <claire@hashicorp.com>
2022-09-20 14:51:17 -0500

    disable other aws tests

--- get.go
+++ get.go
@@ -65,11 +65,11 @@ func init() {
 	}
 
 	Getters = map[string]Getter{
-		"file":  new(FileGetter),
-		"git":   new(GitGetter),
-		"gcs":   new(GCSGetter),
-		"hg":    new(HgGetter),
-		"s3":    new(S3Getter),
+		"file": new(FileGetter),
+		"git":  new(GitGetter),
+		"gcs":  new(GCSGetter),
+		"hg":   new(HgGetter),
+		// "s3":    new(S3Getter),
 		"http":  httpGetter,
 		"https": httpGetter,
 	}
